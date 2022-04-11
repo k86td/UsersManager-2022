d1f07997cd109496f213ea3f81682d03c9f9e5dd
Tristan <40375233+k86td@users.noreply.github.com>
2022-04-11 14:59:44 -0400

    Edited database to start with now datetime. added controller FriendShips.

---
+++ Controllers/FriendShipsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UsersManager.Controllers
+{
+    public class FriendShipsController : Controller
+    {
+        // GET: FriendShips
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
